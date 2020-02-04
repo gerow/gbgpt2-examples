fa8ac978f281bbf28ff0a490097b2e63b6104197
Mike Gerow <gerow@mgerow.com>
2020-02-03 22:51:11 -0500

    examples dir

---
+++ split.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+if __name__ == '__main__':
+    out = 'examples'
+
+    games = sys.stdin.read().split('====================')
+    games = games[1:]
+
+    os.mkdir(out)
+    for i in range(len(games)):
+        o = games[i].strip('===')
+        with open(os.path.join(out, str(i).zfill(3)+'.md'), 'w') as f:
+            f.write(o)
